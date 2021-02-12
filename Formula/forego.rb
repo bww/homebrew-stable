409e70848114dc2f70c487134906f3758cf11e68
Brian W. Wolter <brianwolter@gmail.com>
2021-02-11 20:09:38 -0500

    ...

--- Formula/forego.rb
+++ Formula/forego.rb
@@ -1,9 +1,9 @@
 
 class Forego < Formula
   homepage "https://github.com/bww/forego"
-  url "https://github.com/bww/forego/releases/download/untagged-918103759f1f170913ee/forego-07da109-darwin-amd64.tgz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  version "v0.9.3"
+  url "https://github.com/bww/forego/releases/download/v1.9.4/forego-0e4df30-darwin-amd64.tgz"
+  sha256 "ffd4dd75d9ad1e079670713ebecd7d45d7a6102d69dad936754af461a8f9a9ac"
+  version "v1.9.4"
   
   def install
     bin.install "bin/forego"
