dd2aa0e4d9b587e244f647fba48635d73f25bdd5
Brian W. Wolter <brianwolter@gmail.com>
2021-02-11 22:14:01 -0500

    ...

--- Formula/instaunit.rb
+++ Formula/instaunit.rb
@@ -1,5 +1,5 @@
 
-class Forego < Formula
+class Instaunit < Formula
   homepage "https://github.com/bww/instaunit"
   url "https://github.com/instaunit/instaunit/releases/download/v1.9.1/instaunit-cdbbfa4a759c6bb932ca85ecf97de852808a3b62-darwin-amd64.tgz"
   sha256 "23bd7cad04b5d643d07052d1610355704bf0bf815877100f68b694de8a81add4"
