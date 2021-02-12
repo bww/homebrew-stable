084faf6682def443d69304d4d57cccbca6ee182c
Brian W. Wolter <brianwolter@gmail.com>
2021-02-11 22:08:24 -0500

    Adding Instaunit

---
+++ Formula/instaunit.rb
@@ -0,0 +1,13 @@
+
+class Forego < Formula
+  homepage "https://github.com/bww/instaunit"
+  url "https://github.com/instaunit/instaunit/releases/download/v1.9.1/instaunit-cdbbfa4a759c6bb932ca85ecf97de852808a3b62-darwin-amd64.tgz"
+  sha256 "23bd7cad04b5d643d07052d1610355704bf0bf815877100f68b694de8a81add4"
+  version "v1.9.1"
+  
+  def install
+    bin.install "bin/instaunit"
+  end
+end
+Formula
+
