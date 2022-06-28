1a29f3ab6687f515281317e15103562c6baf33a8
Brian W. Wolter <brianwolter@gmail.com>
2022-06-28 08:58:34 +0200

    Instaunit

--- Formula/instaunit.rb
+++ Formula/instaunit.rb
@@ -1,9 +1,9 @@
 
 class Instaunit < Formula
   homepage "https://github.com/bww/instaunit"
-  url "https://github.com/instaunit/instaunit/releases/download/v1.19.0/instaunit-c1c331818eb0cb1feca0121c4028703485eccc52-darwin-amd64.tgz"
-  sha256 "156aaff0aebbc032163f5458f7b608365146cedaecc3a7b6a3f0edc17dfb2f0f"
-  version "v1.19.0"
+  url "https://github.com/instaunit/instaunit/releases/download/v1.20.0/instaunit-5a4b109e62630f99dea5c2c873b3a444e2d221fc-darwin-amd64.tgz"
+  sha256 "3cef7da302e7b2e4c55f2a8ae7e52953fa3e17df01a07b591311f5e3600a4047"
+  version "v1.20.0"
   
   def install
     bin.install "bin/instaunit"
