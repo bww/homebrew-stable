c0a36595c40739ceb6b2c5d23b15dccb125db4de
Brian W. Wolter <brian@brianwolter.com>
2022-10-26 11:50:26 -0400

    Bumping Instaunit

--- Formula/instaunit.rb
+++ Formula/instaunit.rb
@@ -1,9 +1,9 @@
 
 class Instaunit < Formula
   homepage "https://github.com/bww/instaunit"
-  url "https://github.com/instaunit/instaunit/releases/download/v1.19.0/instaunit-c1c331818eb0cb1feca0121c4028703485eccc52-darwin-amd64.tgz"
-  sha256 "156aaff0aebbc032163f5458f7b608365146cedaecc3a7b6a3f0edc17dfb2f0f"
-  version "v1.19.0"
+  url "https://github.com/instaunit/instaunit/releases/download/v1.22.0/instaunit-c7b3492152a0250b733085214bc2ed85bb21a329-darwin-amd64.tgz"
+  sha256 "f4952f813baa879d746e5ad1d2845df018d42e0c4fe399e49fdd71b0c6d78def"
+  version "v1.22.0"
   
   def install
     bin.install "bin/instaunit"
