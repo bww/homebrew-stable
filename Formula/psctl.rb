13288f540a660a4083e9cd2c903dfab3cfd13a34
Brian W. Wolter <brianwolter@gmail.com>
2023-04-04 12:28:46 -0400

    Adding Process Control

---
+++ Formula/psctl.rb
@@ -0,0 +1,13 @@
+
+class Psctl < Formula
+  homepage "https://github.com/bww/psctl"
+  url "https://github.com/bww/psctl/releases/download/v0.2.5-4/psctl-v0.2.5-4-x86_64-apple-darwin.tgz"
+  sha256 "82c638075294d30236713fc8ccfac1ea4934dc0b184fc9bf50433afad5311b67"
+  version "0.2.5-4"
+  
+  def install
+    bin.install "bin/psctl"
+  end
+end
+Formula
+
