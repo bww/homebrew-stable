a8296632c7f063b9a018f82e899dd5deaf0f5bdd
Brian W. Wolter <brianwolter@gmail.com>
2023-05-27 18:28:30 -0400

    Package Process Control for Arm64

--- Formula/psctl.rb
+++ Formula/psctl.rb
@@ -1,12 +1,19 @@
 
 class Psctl < Formula
   homepage "https://github.com/bww/psctl"
-  url "https://github.com/bww/psctl/releases/download/v0.2.5-12/psctl-v0.2.5-12-x86_64-apple-darwin.tgz"
-  sha256 "078c1cc53562bb87fd89a7af7a822b81cab72e095a00ae51034ef3d403e51ac5"
-  version "0.2.5-12"
+  version "0.2.6"
+  
+  on_arm do
+    url "https://github.com/bww/psctl/releases/download/v0.2.6/psctl-v0.2.6-aarch64-apple-darwin.tgz"
+    sha256 "77b48694a24cb3e6ab07d9e67d1045eaca58246edee4cae4a639ec2772772256"
+  end
+  on_intel do
+    url "https://github.com/bww/psctl/releases/download/v0.2.6/psctl-v0.2.6-x86_64-apple-darwin.tgz"
+    sha256 "4e7ecdf4833cd8b938a45893c7284c158db2cffb3527e76cf5b2be2a3295982a"
+  end
   
   def install
-    bin.install "x86_64-apple-darwin/release/psctl"
+    bin.install "bin/psctl"
   end
 end
 Formula
