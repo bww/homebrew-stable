b2c773104b5c09ac76792c0072f7ac4866e0aa8d
Brian W. Wolter <brianwolter@gmail.com>
2023-05-29 09:40:31 -0400

    Bump urlencode

--- Formula/urlenc.rb
+++ Formula/urlenc.rb
@@ -1,9 +1,16 @@
 
 class Urlenc < Formula
   homepage "https://github.com/bww/urlenc"
-  url "https://github.com/bww/urlencode/releases/download/v1.1.1/urlenc-6c5ecd0f753ba842b8bbbed98da7ca52773cc98e-darwin-amd64.tgz"
-  sha256 "3f1c128445e7e613e5f95742fb6932be06a1ea93f0f82e506b24e9c122b8f22a"
-  version "v1.1.1"
+  version "1.1.2-3"
+  
+  on_arm do
+    url "https://github.com/bww/urlencode/releases/download/v1.1.2-3/urlenc-v1.1.2-3-darwin-arm64.tgz"
+    sha256 "cd4a439168f7789fe7f3c957f2c8cadf1a83f412fcddf0ab9221d5ead4e76c04"
+  end
+  on_intel do
+    url "https://github.com/bww/urlencode/releases/download/v1.1.2-3/urlenc-v1.1.2-3-darwin-amd64.tgz"
+    sha256 "3be71b0d31e25709318c9fe5b489931de427043122deb00fe6d30ec67665a0ec"
+  end
   
   def install
     bin.install "bin/urlenc"
