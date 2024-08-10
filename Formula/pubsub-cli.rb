3e6c453ea1f08ee4d66baa33f8a0c0b688da08a8
Brian W. Wolter <brianwolter@gmail.com>
2024-08-10 18:34:33 -0400

    Bump PubsubCLI

--- Formula/pubsub-cli.rb
+++ Formula/pubsub-cli.rb
@@ -1,15 +1,15 @@
 
 class PubsubCli < Formula
   homepage "https://github.com/bww/pubsub-cli"
-  version "v0.1.0-4"
+  version "v0.1.0-5"
   
   on_arm do
-    url "https://github.com/bww/pubsub-cli/releases/download/v0.1.0-4/pubsub-cli-v0.1.0-4-darwin-arm64.tgz"
-    sha256 "2bfdd25b3918d86c053e7a78d768d3ad0b80a2e0f8c8f3e410b3e752c86ce454"
+    url "https://github.com/bww/pubsub-cli/releases/download/v0.1.0-5/pubsub-cli-v0.1.0-5-darwin-arm64.tgz"
+    sha256 "c29f0cc0d0d234a57d81939a0300fea2e2167754a5d21146274f97392168f9ef"
   end
   on_intel do
-    url "https://github.com/bww/pubsub-cli/releases/download/v0.1.0-4/pubsub-cli-v0.1.0-4-darwin-amd64.tgz"
-    sha256 "5172729072b769fd6511e3bd335c3161cc5ca380dd270dc1a819245bafa0401d"
+    url "https://github.com/bww/pubsub-cli/releases/download/v0.1.0-5/pubsub-cli-v0.1.0-5-darwin-amd64.tgz"
+    sha256 "c4dfe467d86ac1c084d49facc3d3a479030c6bff69fc5ada4c49c01a4dfa292b"
   end
   
   def install
