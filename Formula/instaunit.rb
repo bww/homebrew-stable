aadd4d2e7b74141db55c913e17a87d3167bc212f
Brian W. Wolter <brianwolter@gmail.com>
2024-09-24 18:29:15 -0400

    Bump Instaunit

--- Formula/instaunit.rb
+++ Formula/instaunit.rb
@@ -1,15 +1,15 @@
 
 class Instaunit < Formula
   homepage "https://github.com/bww/instaunit"
-  version "v1.33.0"
+  version "v1.34.0"
   
   on_arm do
-    url "https://github.com/instaunit/instaunit/releases/download/v1.33.0/instaunit-v1.33.0-darwin-arm64.tgz"
-    sha256 "52163ac5848f16bca1f14f4bc50efc0533f3d96a2fbb203c3a6575bd62a0f9af"
+    url "https://github.com/instaunit/instaunit/releases/download/v1.34.0/instaunit-v1.34.0-darwin-arm64.tgz"
+    sha256 "8cf8944695c70648bebf60df170bfb4755863aad829368512812e691d58ebdc3"
   end
   on_intel do
-    url "https://github.com/instaunit/instaunit/releases/download/v1.33.0/instaunit-v1.33.0-darwin-amd64.tgz"
-    sha256 "8abd445b577c4784af811a7fc5d3781e21c8f09880f842595fe75051256cacd8"
+    url "https://github.com/instaunit/instaunit/releases/download/v1.34.0/instaunit-v1.34.0-darwin-amd64.tgz"
+    sha256 "5ececf107f8ce5c137dba4ff069a4fbc392b2146bcd39aa36f524452a8bb7066"
   end
   
   def install
