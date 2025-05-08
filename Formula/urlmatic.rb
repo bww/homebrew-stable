6c2460f8fbedd2cb971a9e1e756d92de570b31fd
Brian W. Wolter <brianwolter@gmail.com>
2025-05-07 22:45:47 -0400

    Bump urlmatic

--- Formula/urlmatic.rb
+++ Formula/urlmatic.rb
@@ -1,15 +1,15 @@
 
 class Urlmatic < Formula
   homepage "https://github.com/bww/urlmatic"
-  version "0.5.1"
+  version "0.6.1"
   
   on_arm do
-    url "https://github.com/bww/urlmatic/releases/download/v0.5.1/urlmatic-v0.5.1-aarch64-apple-darwin.tgz"
-    sha256 "69b9c3008a6302367ba3452e812ba022fad549d56cc0f8137392cf5bf92bac78"
+    url "https://github.com/bww/urlmatic/releases/download/v0.6.1/urlmatic-v0.6.1-aarch64-apple-darwin.tgz"
+    sha256 "9bc3feea64c5a7e4fe18e4041ee2af8879829adaac9d30cd592d502ced29e481"
   end
   on_intel do
-    url "https://github.com/bww/urlmatic/releases/download/v0.5.1/urlmatic-v0.5.1-x86_64-apple-darwin.tgz"
-    sha256 "56991ef4dff0d74c382b3347f1105d2c25ba4696364cee73fc5df7e5ff167ba0"
+    url "https://github.com/bww/urlmatic/releases/download/v0.6.1/urlmatic-v0.6.1-x86_64-apple-darwin.tgz"
+    sha256 "c1921f7384c29a85a11ca2d767c2749a6efe7b684b8ec3d75dadd20fd0c0065a"
   end
   
   def install
