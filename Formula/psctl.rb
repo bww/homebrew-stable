52c1bf833addfd6bfd0ebce761898576a1846f1b
Brian W. Wolter <brianwolter@gmail.com>
2025-05-11 11:51:28 -0400

    Bump PSCTL

--- Formula/psctl.rb
+++ Formula/psctl.rb
@@ -1,15 +1,15 @@
 
 class Psctl < Formula
   homepage "https://github.com/bww/psctl"
-  version "0.7.1"
+  version "0.8.0"
   
   on_arm do
-    url "https://github.com/bww/psctl/releases/download/v0.7.1/psctl-v0.7.1-aarch64-apple-darwin.tgz"
-    sha256 "39788d1e85605727f014b0dcf9c48293e3abce29cf8bdd4adaa3b3deae24bd41"
+    url "https://github.com/bww/psctl/releases/download/v0.8.0/psctl-v0.8.0-aarch64-apple-darwin.tgz"
+    sha256 "2a86fce072aa9261f517aa17562ec033074679bf3aa908a98c45014286f2f875"
   end
   on_intel do
-    url "https://github.com/bww/psctl/releases/download/v0.7.1/psctl-v0.7.1-x86_64-apple-darwin.tgz"
-    sha256 "d7bb2506067085b89d7768e58661198a4b5f53601f8f45f0a76dc8d70835db84"
+    url "https://github.com/bww/psctl/releases/download/v0.8.0/psctl-v0.8.0-x86_64-apple-darwin.tgz"
+    sha256 "affbe8c690a5d8fbc37b198002935574d00bffdf309eeb6430860e1928416a8f"
   end
   
   def install
