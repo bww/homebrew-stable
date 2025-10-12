fbca86ab07b4bc790cb73c7eb03fc86cdc6fdabe
Brian W. Wolter <brianwolter@gmail.com>
2025-10-12 18:53:07 -0400

    Fixing urls

--- Formula/instaunit.rb
+++ Formula/instaunit.rb
@@ -4,12 +4,12 @@ class Instaunit < Formula
   version "1.39.0"
   
   on_arm do
-    url "https://github.com/instaunit/instaunit/releases/download/v1.39.0/instaunit-v1.39.0-aarch64-apple-darwin.tgz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/instaunit/instaunit/releases/download/v1.39.0/instaunit-v1.39.0-darwin-arm64.tgz"
+    sha256 "07ada166dd0da4fca2c92ea7fd3e85ad85d6a41723402cd1b677d0d9e687b3b1"
   end
   on_intel do
-    url "https://github.com/instaunit/instaunit/releases/download/v1.39.0/instaunit-v1.39.0-x86_64-apple-darwin.tgz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/instaunit/instaunit/releases/download/v1.39.0/instaunit-v1.39.0-darwin-amd64.tgz"
+    sha256 "665e3f1e39a81ceac11635d2c356e2cb3f973d93a7cdb2c8b106455c90b766b8"
   end
   
   def install
