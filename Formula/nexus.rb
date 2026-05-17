eecd2fb45dff36731feaf855fb389c039c985c47
Brian W. Wolter <brianwolter@gmail.com>
2026-05-17 19:46:14 -0400

    Bump nexus

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,15 +1,15 @@
 
 class Nexus < Formula
   homepage "https://github.com/bww/nexus"
-  version "0.1.1"
+  version "0.1.2"
   
   on_arm do
-    url "https://github.com/bww/nexus/releases/download/v0.1.1/nexus-v0.1.1-aarch64-apple-darwin.tgz"
-    sha256 "f4b9bd8b7a018cff413b6523e19c30e89d44a0c9d5020f3ae3edd7b8dc308afd"
+    url "https://github.com/bww/nexus/releases/download/v0.1.2/nexus-v0.1.2-aarch64-apple-darwin.tgz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
   on_intel do
-    url "https://github.com/bww/nexus/releases/download/v0.1.1/nexus-v0.1.1-x86_64-apple-darwin.tgz"
-    sha256 "4057182566addeb00265c23519088816e7cd0519a11cb68de4aa020e16631b06"
+    url "https://github.com/bww/nexus/releases/download/v0.1.2/nexus-v0.1.2-x86_64-apple-darwin.tgz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
   
   def install
